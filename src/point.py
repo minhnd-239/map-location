75a6c13ef5ef8add0e277cb312ff0db6acd0cc2e
minhnd-239 <minhnd239@gmail.com>
2019-11-19 14:12:32 +0700

    fix bug: cannot save feature point

--- src/point.py
+++ src/point.py
@@ -4,59 +4,56 @@ import glob
 import os
 from dask.array.tests.test_numpy_compat import dtype
 # alist =[]
-# count = 0
+count = 1
 list_matches = []
-# data = "Image/test"
-# data_path = os.path.join(data, '*.jpg')
-# file = glob.glob("Image/test/*.jpg")
-# for img in file:
-#     train = cv2.imread(img, cv2.IMREAD_GRAYSCALE)  
+# 
+# for filename in glob.glob('Image/test/*.jpg'):
+# #     print(filename[-7:-4])
+#          
+#     train = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+# #     cv2.imwrite("Image/10/" + "%s.jpg" %filename[-7:-4], train)
+# #     cv2.imshow("train", train)
+#     #cv2.waitKey(0)
 #     orb = cv2.ORB_create()
 #     kp2, des2 = orb.detectAndCompute(train, None)
-#     file_name = "IMG_" + str(count) + ".txt"
-#     with open ("Image/15/" +file_name, "a") as f:
-# #         f.write(str(kp2))
-# #         f.write(";")
-#         if des2 is not None:
-#             for i in des2:
-#                 f.write(str(i))
-#             f.close()
+#     file_name = "Image/15/IMG_" + str(count)
+#     if des2 is not None:
+#         np.save(file_name, des2)
 #     count+=1
+
+
 def compare_feature(des):
-    data = "Image/15"
-    data_path = os.path.join(data, '*.txt')
-    file_name = glob.glob(data_path)
-    temp =[]
-    for file in file_name:
-        with open (file, "r") as file:
-            contents = file.read()
-#             print(contents)
-            temp = np.array((contents))
-            print(contents)
-            bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-#             print(contents)
-            matches = bf.match(des, temp)
-#             print(matches)
-            list_matches.append(matches)
-        file.close()
-#             contents = txt.split(";")
-            
+  
+    for filename in glob.glob('Image/15/*.npy'):
+        print(filename)
+        contents = np.load(filename) # load
+#         print(contents)
+        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    #             print(contents)
+        matches = bf.match(des, contents)
+#         print(len(matches))
+        list_matches.append(len(matches))
+
+
             
             
     
   
     
 print('ok')
-query = cv2.imread("IMG_0448 011.jpg", cv2.IMREAD_GRAYSCALE)
-# # train = cv2.imread("IMG_0448 001.jpg", cv2.IMREAD_GRAYSCALE)
-# # query = cv2.resize(query, (500,500))
-# # train = cv2.resize(train, (500,500))
+query = cv2.imread("Image/10/010.jpg", cv2.IMREAD_GRAYSCALE)
+# cv2.imshow("query", query)
+# train = cv2.imread("Image/10/013.jpg", cv2.IMREAD_GRAYSCALE)
+# query = cv2.resize(query, (500,500))
+# train = cv2.resize(train, (500,500))
 orb = cv2.ORB_create()
-#    
+#  
 kp1, des1 = orb.detectAndCompute(query, None)
+# kp2, des2 = orb.detectAndCompute(train, None)
 compare_feature(des1)
 print(list_matches)
-# # kp2, des2 = orb.detectAndCompute(train, None)
+print(max(list_matches))
+# kp2, des2 = orb.detectAndCompute(train, None)
 # print(des2)
 # for i in des2:
 #     print(str(i))
